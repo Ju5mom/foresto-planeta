dc43ca489ba30118bc90b55fe867017486f5344a
Ju5mom <m.ishibashi@enjoyworks.jp>
2026-03-09 15:49:02 +0900

    add_sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   site: 'https://ju5mom.github.io',
-  base: '/foresto-planeta/'
+  base: '/foresto-planeta/',
 });
